f2a6ce9e39c319182ced3f8c37c4df814b0f256f
Kevin <krodriguez@horinc.com>
2022-10-25 10:12:09 -0400

    adding more CRUD operations

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -2,7 +2,8 @@ from time import timezone
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from blog.forms import PostForm, CommentForm
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from blog.models import Post, Comment
 # Create your views here.
 
@@ -13,7 +14,7 @@ class PostListView(ListView):
     model = Post
     
     def get_queryset(self):
-        return Post.obects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -23,3 +24,21 @@ class CreatePostView(CreateView, LoginRequiredMixin):
     redirect_field_name = 'blog/post/detail.html'
     form_class = PostForm
     model = Post
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post/detail.html'
+    form_class = PostForm
+    model = Post
+    
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    success_url = reverse_lazy('post_list')
+    model = Post
+
+class DraftListView(ListView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+    
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
