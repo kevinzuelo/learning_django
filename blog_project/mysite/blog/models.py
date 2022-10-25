2bf61e1a484717ad71206a95cbf2107bde16f208
Kevin <krodriguez@horinc.com>
2022-10-25 15:25:40 -0400

    fixed not null contraint bug

--- blog_project/mysite/blog/models.py
+++ blog_project/mysite/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(timezone.now())
+    create_date = models.DateTimeField(timezone.now(), null=True)
     published_date = models.DateTimeField(blank=True, null=True)
     
     def publish(self):
@@ -28,7 +28,7 @@ class Comment(models.Model):
     post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(timezone.now())
+    create_date = models.DateTimeField(timezone.now(), null=True)
     approved_comment = models.BooleanField(default='False')
     
     def approve(self):
